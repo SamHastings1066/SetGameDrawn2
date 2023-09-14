40155f778df42d1bdc58c1705ac181af3dc93bb4
Sam Hastings <samhastingsis@gmail.com>
2023-09-14 13:50:36 -0400

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by sam hastings on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
